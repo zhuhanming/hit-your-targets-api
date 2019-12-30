497b7f9b9eb2d239ac7f0c859510f48eb5736429
Hanming Zhu <hanming@huawen.com>
2019-12-30 12:26:17 +0800

    chore: set up pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_29_162734) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "subtodos", force: :cascade do |t|
     t.string "title"
     t.datetime "start_time"
