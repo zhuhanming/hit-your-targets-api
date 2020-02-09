cdf28637a299cc9e8c3fdc4eec536183bc0bf946
Hanming Zhu <hanming@huawen.com>
2020-02-09 11:25:44 +0800

    feat: add base url render to check if api is up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::API
 
   # called before every action on controllers
   before_action :authorize_request
+  skip_before_action :authorize_request, only: :show
   attr_reader :current_user
+
+  def show
+    render plain: "Hit Your Targets API is up and running. Get started now at www.hityourtargets.xyz"
+  end
 
   private
 
